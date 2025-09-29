d762bc2ff77e8b70cc7d58639a2f792def5fde8a
Pavel Khmara <375295560023pk@gmail.com>
2025-09-29 18:58:45 +0200

    logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function POST() {
   (await cookies()).delete("clientId");
   (await cookies()).delete("clientKey");
   (await cookies()).delete("clientName");
-  return NextResponse.json({ ok: true });
+  NextResponse.json({ ok: true });
+  return redirect("/");
 }
